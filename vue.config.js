514d3fdb2525d3cb00d1dad82d9b6272622d01f9
ChanningKuo <492689363@qq.com>
2021-07-05 21:57:08 +0800

    change base url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/huahua/',
+  // / huahua
+  publicPath: process.env.NODE_ENV !== 'production' ? '/' : '/',
   // publicPath: '../',
 
   productionSourceMap: process.env.NODE_ENV !== 'production',
